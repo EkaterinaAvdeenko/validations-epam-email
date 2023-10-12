977b6021197f5233bc18430bdac72ec8e47c1e8f
Ekaterina Avdeenko <avdeenqoeka@gmail.com>
2023-10-12 11:23:52 +0000

    Update EpamEmailValidation.java

--- src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
@@ -6,12 +6,9 @@ public class EpamEmailValidation {
     public static boolean validateEpamEmail(String email) {
         String regex = "^[a-zA-Z]+[_]+[a-zA-Z0-9]+@epam.com$";
         Pattern p = Pattern.compile(regex);
-        // Если строка пустая, то вернуть ноль
-        // вернуть false
         if (email == null) {
             return false;
         }
-        // Pattern class содержит matcher() метод
         Matcher m = p.matcher(email);
         return m.matches();
     }
