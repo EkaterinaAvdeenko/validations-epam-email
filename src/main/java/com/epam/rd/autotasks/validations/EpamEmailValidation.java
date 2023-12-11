43f4c4c43f566b4fadfc75ba93fda68be27056f2
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-12-11 19:38:10 +0100

    validations-epam-email-AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
@@ -2,16 +2,15 @@ package com.epam.rd.autotasks.validations;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 public class EpamEmailValidation {
-    public static boolean validateEpamEmail(String email) {
-        String regex = "^[a-zA-Z]+[_]+[a-zA-Z0-9]+@epam.com$";
-        Pattern p = Pattern.compile(regex);
-        if (email == null) {
-            return false;
-        }
-        Matcher m = p.matcher(email);
-        return m.matches();
-    }}
-
-
-
+  public static boolean validateEpamEmail(String email) {
+    String regex = "^[a-zA-Z]+[_]+[a-zA-Z0-9]+@epam.com$";
+    Pattern p = Pattern.compile(regex);
+    if (email == null) {
+      return false;
+    }
+    Matcher m = p.matcher(email);
+    return m.matches();
+  }
+}
--- src/test/java/com/epam/rd/autotasks/validations/EpamEmailValidationTest.java
+++ src/test/java/com/epam/rd/autotasks/validations/EpamEmailValidationTest.java
@@ -10,34 +10,36 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class EpamEmailValidationTest {
 
-    @ParameterizedTest(name = "{index} \"{0}\" is not a valid EPAM email")
-    @NullAndEmptySource
-    @ValueSource(strings = {
-            " ",
-            "william@epam.com",
-            "william.shakespeare@epam.com",
-            "william...shakespeare@epam.com",
-            "william-shakespeare@epam.com",
-            "shakespeare123@epam.com",
-            "william_$hakespeare@epam.com",
-            "william_shakespeare@epam.org",
-            "_shakespeare@epam.com",
-            "shakespeare_@epam.com",
-            "william.shakespeare@epam@com",
-            "william.shakespeare@epamcom",
-    })
-    void validateEpamEmailInvalidCases(String input) {
-        assertFalse(validateEpamEmail(input));
-    }
+  @ParameterizedTest(name = "{index} \"{0}\" is not a valid EPAM email")
+  @NullAndEmptySource
+  @ValueSource(
+      strings = {
+        " ",
+        "william@epam.com",
+        "william.shakespeare@epam.com",
+        "william...shakespeare@epam.com",
+        "william-shakespeare@epam.com",
+        "shakespeare123@epam.com",
+        "william_$hakespeare@epam.com",
+        "william_shakespeare@epam.org",
+        "_shakespeare@epam.com",
+        "shakespeare_@epam.com",
+        "william.shakespeare@epam@com",
+        "william.shakespeare@epamcom",
+      })
+  void validateEpamEmailInvalidCases(String input) {
+    assertFalse(validateEpamEmail(input));
+  }
 
-    @ParameterizedTest(name = "{index} \"{0}\" is not a valid EPAM email")
-    @ValueSource(strings = {
-            "william_shakespeare@epam.com",
-            "lu_e@epam.com",
-            "william_shakespeare1@epam.com",
-            "william_shakespeare2@epam.com"
-    })
-    void validateEpamEmailValidCases(String input) {
-        assertTrue(validateEpamEmail(input));
-    }
+  @ParameterizedTest(name = "{index} \"{0}\" is not a valid EPAM email")
+  @ValueSource(
+      strings = {
+        "william_shakespeare@epam.com",
+        "lu_e@epam.com",
+        "william_shakespeare1@epam.com",
+        "william_shakespeare2@epam.com"
+      })
+  void validateEpamEmailValidCases(String input) {
+    assertTrue(validateEpamEmail(input));
+  }
 }
